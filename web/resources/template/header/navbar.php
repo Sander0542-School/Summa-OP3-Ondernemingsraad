fe0cf1b89f692da8daba67fa78e68d021095c9ed
Sander0542 <sanderjochems@hotmail.nl>
2019-02-07 12:13:19 +0100

    Update navbar.php

--- web/resources/template/header/navbar.php
+++ web/resources/template/header/navbar.php
@@ -6,9 +6,10 @@
         <li><a href="" class="brand-logo">Ondernemingsraad</a></li>
       </ul>
       <ul class="right">
-        <li><a href="overzicht.php">overzicht</a></li>
-        <li><a href="stemmen.php">stemmen</a></li>
-        <li><a href="verkiesbaar.php">verkiesbaar</a></li>
+        <li><a href="/overzicht">Overzicht</a></li>
+        <li><a href="/stemmen">Stemmen</a></li>
+        <li><a href="/verkiesbaar">Verkiesbaar</a></li>
+        <li class="navbar-empty"><a href=""></a></li>
       </ul>
     </div>
   </nav>
